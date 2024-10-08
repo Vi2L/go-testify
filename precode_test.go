3eab22faa4766cea4aeb3f38f98629adc7ea7da8
Vitaliy Yarchuk <vlyarchuk@yandex.ru>
2024-10-08 16:54:20 +0300

    2-st commit

--- precode_test.go
+++ precode_test.go
@@ -10,45 +10,53 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMainHandlerWhenEverythingIsOK(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	body := responseRecorder.Body.String()
-	assert.NotEmpty(t, body)
-}
-
-func TestMainHandlerWhenCityIsWrong(t *testing.T) {
-	errorText := "wrong city value"
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=chelyabinsk", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-
-	body := responseRecorder.Body.String()
-	assert.Equal(t, errorText, body)
-}
-
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=6&city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	body := responseRecorder.Body.String()
-	assert.NotEmpty(t, body)
+	//Проверка статус кода, отличный от 200
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusOK, status)
 
+	//Получаем список кафе
+	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
 
+	//Проверяем, что длина полученных кафе равна ожидаемой
 	assert.Len(t, list, totalCount)
+}
+
+func TestMainHandlerWhenOkAndBodyNotEmpty(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//Проверка статус кода, отличный от 200
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusOK, status)
+
+	//Проверяем, что тело ответа не пустое
+	assert.NotEmpty(t, responseRecorder.Body)
+}
+
+func TestMainHandlerWhenMissingCount(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//Проверка статус кода, отличный от 400
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusBadRequest, status)
+
+	//Проверка ожидаемого ответа сервера
+	expected := "count missing"
+	assert.Equal(t, expected, responseRecorder.Body.String())
 }
